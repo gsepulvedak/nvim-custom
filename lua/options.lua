47517b15de2462621e4385a07313c5e419bcb6ce
Gonzalo Sepulveda <gsepulvedak@gmail.com>
2025-10-09 10:48:45 -0300

    highlight cursor line and yanked text

--- lua/options.lua
+++ lua/options.lua
@@ -32,3 +32,27 @@ vim.opt.ignorecase = true
 -- Enable term colors (however it didn't make a change)
 vim.opt.termguicolors = true
 
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Set diagnostics to show as virtual text on current line (>= 0.11)
+vim.diagnostic.config({
+  virtual_text = { current_line = true },
+})
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
