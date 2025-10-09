522d2b697b1c754fdd29300025c9d11f1befad66
Gonzalo Sepulveda <gsepulvedak@gmail.com>
2025-10-09 10:46:47 -0300

    load general keymaps and db-connections for dadbod-ui

--- init.lua
+++ init.lua
@@ -7,8 +7,14 @@ vim.g.maplocalleader = " "
 -- Load nvim options
 require("options")
 
+-- Load keymaps
+require("keymaps")
+
 -- Load lsp servers. To add more servers, edit ./lua/lsp.lua
 require("lsp")
 
 -- Install or load lazy.lua
 require("config.lazy")
+
+-- Configure database connections for dadbod-ui
+require("db-connections")
