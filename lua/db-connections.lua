29c616510143f91a310ad275e9522d5c2aed6e23
Gonzalo Sepulveda <gsepulvedak@gmail.com>
2026-07-02 12:07:01 -0400

    abstract infrastructure metadata

--- lua/db-connections.lua
+++ lua/db-connections.lua
@@ -1,96 +1,33 @@
--- Keep passwords out of the URI so libpq/psql can read them from ~/.pgpass
--- or $PGPASSFILE without worrying about URL escaping.
-local function postgres_url(opts)
-  local query = ""
-  if opts.params and next(opts.params) then
-    local pairs_list = {}
-    for key, value in pairs(opts.params) do
-      pairs_list[#pairs_list + 1] = { key = key, value = value }
-    end
-    table.sort(pairs_list, function(a, b)
-      return a.key < b.key
-    end)
-
-    local query_parts = {}
-    for i = 1, #pairs_list do
-      local item = pairs_list[i]
-      query_parts[#query_parts + 1] = ("%s=%s"):format(item.key, vim.uri_encode(item.value))
-    end
-    query = "?" .. table.concat(query_parts, "&")
-  end
-
-  return ("postgresql://%s@%s/%s%s"):format(
-    vim.uri_encode(opts.user),
-    opts.host,
-    vim.uri_encode(opts.database),
-    query
-  )
+-- Keep connection metadata in public-safe service names.
+-- Real host/db settings live in ~/.pg_service.conf and passwords in ~/.pgpass
+-- (or $PGSERVICEFILE / $PGPASSFILE) so Neovim never has to embed secrets.
+local function postgres_service_url(service)
+  return ("postgresql:///?service=%s"):format(vim.uri_encode(service))
 end
 
 vim.g.dbs = {
   {
     name = "SEIA-DEV",
-    url = postgres_url({
-      user = "root",
-      host = "smartcompliance-prod-datascience-rds-dlux-database-3nypvrwwkodo.clqmqgma84gw.us-east-1.rds.amazonaws.com",
-      database = "prodDBsmartcomplianceDataScience",
-    }),
+    url = postgres_service_url("seia_dev"),
   },
   {
     name = "SEIA-PROD",
-    url = postgres_url({
-      user = "root",
-      host = "smartcompliance-prod2-datascience-rds-1pv-database-mrshebsb6obz.ctsocesoy3lm.us-east-2.rds.amazonaws.com",
-      database = "prodDBsmartcomplianceDataScience",
-      params = {
-        sslmode = "require",
-      },
-    }),
+    url = postgres_service_url("seia_prod"),
   },
   {
     name = "SNIFA-DEV",
-    url = postgres_url({
-      user = "root",
-      host = "127.0.0.1:8081",
-      database = "smartCompliancesSnifa",
-      params = {
-        sslmode = "require",
-      },
-    }),
+    url = postgres_service_url("snifa_dev"),
   },
   {
     name = "SNIFA-PERT",
-    url = postgres_url({
-      user = "root",
-      host = "127.0.0.1:8081",
-      database = "smartCompliancesSeia",
-      params = {
-        sslmode = "require",
-      },
-    }),
+    url = postgres_service_url("snifa_pert"),
   },
   {
     name = "reclamaciones_dev",
-    url = postgres_url({
-      user = "neondb_owner",
-      host = "ep-dark-salad-aagsermr-pooler.westus3.azure.neon.tech",
-      database = "neondb",
-      params = {
-        channel_binding = "require",
-        sslmode = "require",
-      },
-    }),
+    url = postgres_service_url("reclamaciones_dev"),
   },
   {
     name = "reclamaciones_prod",
-    url = postgres_url({
-      user = "neondb_owner",
-      host = "ep-young-cloud-aaipsvch-pooler.westus3.azure.neon.tech",
-      database = "neondb",
-      params = {
-        channel_binding = "require",
-        sslmode = "require",
-      },
-    }),
+    url = postgres_service_url("reclamaciones_prod"),
   },
 }
